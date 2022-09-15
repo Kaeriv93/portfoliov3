fa13524ce0e07b7c2a3ae7cf37385d1b3572c878
kaeriv93 <vsan93@yahoo.com>
2022-09-15 13:21:27 -0500

    installed typewrite animation and incorporated it in portfolio

--- src/components/heroimg/HeroImg.jsx
+++ src/components/heroimg/HeroImg.jsx
@@ -1,5 +1,6 @@
 import './HeroImg.styles.scss';
 import IntroImg from "../../assets/intro-pic.jpeg";
+import { Typewriter, Cursor} from 'react-simple-typewriter'
 import{ Link } from 'react-router-dom'
 const HeroImg = () =>{
     return(
@@ -9,7 +10,18 @@ const HeroImg = () =>{
             </div>
             <div className="content">
                 <p>Hi, I'm Vireak</p>
-                <h1>React Developer.</h1>
+                <h1>{''}
+                    <Typewriter
+                        words={['React.', 'Express.', 'Javascript.', 'Node.js.']}
+                        loop = {false}
+                        cursor
+                        cursorStyle='_'
+                        typeSpeed={100}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                    <Cursor/>
+                </h1>
                 <div>
                     <Link to ='/project' className='btn'>Projects</Link>
                     <Link to ='/contact' className='btn btn-light'>Contact</Link>
