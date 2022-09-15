4eefd1ffca10385f26a3e5624028b0e351d51c10
kaeriv93 <vsan93@yahoo.com>
2022-09-14 23:16:23 -0500

    fixed some small bugs

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Home from "./routes/home/Home";
 import Project from "./routes/project/Project";
 import { Routes, Route } from 'react-router-dom';
 
+
 function App() {
   return (
     <Routes>
-      <Route path = '/' element ={<Home/>}/>
+      <Route path ='/' element ={<Home/>}/>
       <Route path ='/about' element = {<About/>}/>
       <Route path ='/contact' element  = {<Contact/>}/>
       <Route path ='/project' element  = {<Project/>}/>
